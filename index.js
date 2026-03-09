20d34b2d92a775a7bffc10148b945d0e154bb5cc
Plinio Peixoto dos Santos <plinio.santos@ssa-br.com>
2026-03-09 14:34:16 -0300

    fim ?

--- index.js
+++ index.js
@@ -8,20 +8,25 @@ let tolerancia = pesoNota * 0.0025;
 let quebra = pesoNota - pesoFinal;
 let percentual = (quebra / pesoNota) * 100;
 
+let excesso = Math.abs(quebra) - tolerancia;
+
 let resultado = document.getElementById("resultado");
 
 if (pesoFinal > pesoNota + tolerancia || pesoFinal < pesoNota - tolerancia){
-
 resultado.innerText =
 `Houve uma quebra de ${quebra.toFixed(2)} kg.
 Poderia dar uma diferença de no máximo ${tolerancia.toFixed(2)} kg.
-A diferença foi ${quebra.toFixed(2)} kg.
-Isso representa ${percentual.toFixed(3)}% da carga.`;
+A diferença acima do permitido foi ${excesso.toFixed(2)} kg.
+Isso representa ${percentual.toFixed(2)}% da carga.`;
 
 }else{
-
 resultado.innerText = "Peso dentro da tolerância.";
-
 }
 
+} 
+
+
+function reiniciar(){
+    document.getElementById("formulario").reset();
+    document.getElementById("resultado").innerText = "";
 }
